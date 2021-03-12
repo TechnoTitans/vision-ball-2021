4227a6e736653a2cba82b1f214aa9ed7386f2557
Cyrus <cyrusyong1@gmail.com>
2021-03-12 12:57:08 -0500

    basic ball detection

---
+++ Ball Detection.py
@@ -0,0 +1,38 @@
+#Untitled - By: HP - Mon Mar 1 2021
+
+import sensor, image, time
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QQVGA)
+sensor.skip_frames(time = 2000)
+
+clock = time.clock()
+
+KMAN = 0.3 # constant for exposure setting
+autoExposureSum = 0
+readExposureNum = 10
+for i in range(readExposureNum):
+    autoExposureSum += sensor.get_exposure_us()
+
+autoExposure = autoExposureSum/readExposureNum
+manualExposure = int(autoExposure * KMAN) # scale factor for decreasing autoExposure
+sensor.set_auto_exposure(False,  manualExposure)
+
+yellow_threshold_BBG = [(25, 88, -128, 24, 1, 127)] #Dark Background
+yellow_threshold_WBG = [(30, 100, -20, 127, 20, 127)] #Light Background
+
+while(True):
+    clock.tick()
+
+    img = sensor.snapshot()
+
+    for blobs in img.find_blobs(yellow_threshold_BBG):
+        if(blobs.roundness() > 0.77 and blobs.pixels() > 250):
+            img.draw_rectangle(blobs.x(), blobs.y(), blobs.w(), blobs.h(), (255, 0, 0), 2)
+    """
+    for blobs in img.find_blobs(yellow_threshold_WBG):
+        if(blobs.roundness() > 0.77 and blobs.pixels() > 250):
+            img.draw_rectangle(blobs.x(), blobs.y(), blobs.w(), blobs.h(), (255, 0, 0), 2)
+    """
+
